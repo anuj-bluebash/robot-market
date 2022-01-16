afb95acbaa3aff55b0822f595626483d5c6e41e7
Sheriff <anuj@bluebash.co>
2022-01-16 13:23:53 +0530

    Material filter and get uniq matirials from data

--- frontend/src/components/collections-overview/collections-overview.component.jsx
+++ frontend/src/components/collections-overview/collections-overview.component.jsx
@@ -1,8 +1,8 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollections } from '../../redux/shop/shop.selectors'
+import { selectCollections, selectMaterials } from '../../redux/shop/shop.selectors'
 import { productsLoad } from '../../redux/shop/shop.actions'
 
 import CollectionItem from '../collection-item/collection-item.component';
@@ -10,20 +10,32 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collections.overview.styles.scss'
 
-const CollectionsOverview = ({ collections, productsLoad }) => {
+const CollectionsOverview = ({ collections, materials, productsLoad }) => {
+  const [material, setMaterial] = useState('');
+
   useEffect((props) => {
     productsLoad()
   }, [])
+
   return(
-    <div className={'collections-overview'}>
-      {collections.map((item) => (
-        <CollectionItem key={item.name} item={item} />
-      ))}
+    <div className="collections-overview">
+      <select onChange={(e)=> setMaterial(e.target.value)}>
+        <option selected value=''>Select Material</option>
+        {materials.map((material) => (
+          <option value={material}>{material}</option>
+        ))}
+      </select>
+      <div className="items">
+        {collections.filter(item => material == '' || item.material === material).map((item) => (
+          <CollectionItem key={item.name} item={item} />
+        ))}
+      </div>
     </div>
   )}
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections,
+  collections: selectCollections,
+  materials: selectMaterials
 })
 
 const mapDispatchToProps = dispatch => ({
--- frontend/src/redux/shop/shop.selectors.js
+++ frontend/src/redux/shop/shop.selectors.js
@@ -3,6 +3,11 @@ import { createSelector } from 'reselect'
 const selectShop = state => state.shop
 
 export const selectCollections = createSelector(
-    [selectShop],
-    shop => shop.collections
+  [selectShop],
+  shop => shop.collections
+);
+
+export const selectMaterials = createSelector(
+  [selectShop],
+  shop => [...new Set(shop.collections.map(item => item.material))]
 );
